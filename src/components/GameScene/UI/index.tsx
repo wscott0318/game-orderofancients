acb0f8a37c987842c0a905a6c9746eb9d40cf013
pitervela <pitervelasquez03@gmail.com>
2023-05-11 16:55:35 -0400

    ADD UI component

---
+++ src/components/GameScene/UI/index.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+import "./index.scss";
+
+export const UI = () => {
+    return (
+        <div className="content">
+            <button>STOP</button>
+            <button>PLAY</button>
+        </div>
+    );
+};
