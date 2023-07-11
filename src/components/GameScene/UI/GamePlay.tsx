50d3e741fa76c63b30d6a8e4812bd785e6290274
pitervela <pitervelasquez03@gmail.com>
2023-07-11 14:30:00 -0400

    update the map

--- src/components/GameScene/UI/GamePlay.tsx
+++ src/components/GameScene/UI/GamePlay.tsx
@@ -7,6 +7,8 @@ import { Game } from "../game";
 const GamePlay = styled.div`
     position: fixed;
     z-index: 10;
+    width: 100vw;
+    height: 100vh;
 
     .plant {
         background-image: url("/assets/images/bottom-plant.png");
@@ -67,16 +69,8 @@ const GamePlay = styled.div`
     }
 
     .spec {
-        // position: relative;
         display: flex;
         flex-wrap: wrap;
-        // gap: 0.26vw;
-        // padding: 0.1vw 0.25vw;
-        // width: 16.5%;
-        // height: fit-content;
-        // max-height: 77%;
-        // top: 20%;
-        // left: 70%;
         .item {
             border: solid 0.1vw darkgray;
             width: 23%;
@@ -122,6 +116,196 @@ const GamePlay = styled.div`
             font-size: 20px;
         }
     }
+
+    .detail {
+        width: 288px;
+        height: 173px;
+        position: absolute;
+        background-color: #0004;
+        border-radius: 5px;
+        border-width: 2px;
+        border-color: #2c322f;
+        display: flex;
+        flex-direction: column;
+        color: white;
+
+        .detail_title {
+            height: 18%;
+            display: flex;
+            justify-content: space-between;
+            padding-left: 5%;
+            padding-right: 3%;
+            font-size: 15px;
+            display: flex;
+            justify-content: space-between;
+            padding-left: 5%;
+            padding-right: 3%;
+            align-items: center;
+
+            .price {
+                height: 100%;
+                display: flex;
+                align-items: center;
+                img {
+                    height: 80%;
+                    margin-right: 10px;
+                }
+            }
+        }
+        .detail_data {
+            height: 70%;
+            border-top-width: 2px;
+            border-top-color: #2c322f;
+            font-size: 13px;
+            display: flex;
+            flex-direction: column;
+            padding-left: 5%;
+
+            p {
+                height: 14%;
+                display: flex;
+                align-items: center;
+            }
+
+            p:nth-child(1) {
+                height: 20%;
+                font-size: 18px;
+            }
+            p:nth-child(7) {
+                margin-top: 4%;
+            }
+            span {
+                margin-left: 5%;
+            }
+        }
+
+        @media only screen and (max-width: 1920px) {
+            width: 15vw;
+            height: 9vw;
+
+            .detail_title {
+                font-size: 0.78vw;
+            }
+            .detail_data {
+                font-size: 0.67vw;
+                p:nth-child(1) {
+                    font-size: 0.93vw;
+                }
+            }
+        }
+    }
+
+    .map {
+        position: absolute;
+        background-image: url("/assets/images/map-back.png");
+        width: 24vw;
+        height: 13vw;
+        background-size: 100% 100%;
+        bottom: 0;
+
+        @media only screen and (min-width: 1920px) {
+            width: 467px;
+            height: 257px;
+        }
+    }
+    .self {
+        position: absolute;
+        width: 4.15vw;
+        margin: 1.03vw;
+
+        .health {
+            margin-top: 0.13vw;
+            height: 0.26vw;
+            background-color: red;
+            border-radius: 10px;
+        }
+        .mana {
+            margin-top: 0.13vw;
+            height: 0.26vw;
+            background-color: blue;
+            border-radius: 10px;
+        }
+        @media only screen and (min-width: 1920px) {
+            width: 80px;
+            margin: 20px;
+
+            .health {
+                margin-top: 3px;
+                height: 5px;
+            }
+            .mana {
+                margin-top: 3px;
+                height: 5px;
+            }
+        }
+    }
+
+    .player_stats {
+        position: absolute;
+        display: flex;
+        flex-direction: column;
+
+        right: 67px;
+        top: 49px;
+        width: 401px;
+
+        div {
+            background-color: #0004;
+            border-radius: 5px;
+            border-width: 2px;
+            border-color: #2c322f;
+            right: 0px;
+        }
+
+        .time {
+            align-self: flex-end;
+            color: #e9e502;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            padding-left: 5%;
+            padding-right: 5%;
+            width: 67%;
+
+            height: 30px;
+        }
+
+        .stats {
+            width: 100%;
+            color: #e9e502;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            position: relative;
+
+            top: 10px;
+            height: 30px;
+
+            .switch {
+                position: absolute;
+                right: 3%;
+                cursor: pointer;
+            }
+        }
+        .players {
+            position: relative;
+
+            top: 15px;
+
+            th {
+                color: #919056;
+            }
+        }
+    }
+`;
+
+const YellowBoldFont = styled.span`
+    color: #e9e502;
+    font-size: 15px;
+
+    @media only screen and (max-width: 1920px) {
+        font-size: 0.78vw;
+    }
 `;
 
 // const Gold = styled.div`
@@ -145,8 +329,18 @@ interface GamePlayUIProps {
         current: Game;
     };
 }
-
+interface PlayerData {
+    name: string;
+    color: string;
+    level: number;
+    kills: number;
+    income: number;
+    wins: number;
+    lastStands: number;
+}
 const GamePlayUI = ({ gameRef }: GamePlayUIProps) => {
+    const [detailPos, setDetailPos]: [any, any] = useState({ x: 0, y: 0 });
+
     // const playControlDown = gsap.timeline();
 
     const [upgrades, setUpgrades] = useState([
@@ -166,6 +360,36 @@ const GamePlayUI = ({ gameRef }: GamePlayUIProps) => {
         SPELLS_INFO["Chaos_Claw"],
         SPELLS_INFO[`Philosopher's Stone`],
         SPELLS_INFO["Bow"],
+    ]);
+    const [playerShow, setPlayerShow]: [boolean, any] = useState(true);
+    const [players, setPlayers]: [PlayerData[], any] = useState([
+        {
+            name: "Jack#2643",
+            color: "red",
+            level: 100,
+            kills: 5,
+            income: 2,
+            wins: 0,
+            lastStands: 0,
+        },
+        {
+            name: "Player2",
+            color: "blue",
+            level: 100,
+            kills: 5,
+            income: 2,
+            wins: 0,
+            lastStands: 0,
+        },
+        {
+            name: "Player3",
+            color: "pink",
+            level: 100,
+            kills: 5,
+            income: 2,
+            wins: 0,
+            lastStands: 0,
+        },
     ]);
     // useEffect(() => {
     //     playControlDown
@@ -216,53 +440,28 @@ const GamePlayUI = ({ gameRef }: GamePlayUIProps) => {
 
         setUpgrades(newUpgrades);
     };
+    const setDetail = () => {
+        const fieldBounding = document
+            .querySelector(".field")
+            ?.getBoundingClientRect() as any;
+        setDetailPos({ x: fieldBounding.right + 10, y: fieldBounding.top });
+    };
+    useEffect(() => {
+        setDetail();
+        window.addEventListener("resize", setDetail);
+    }, []);
 
+    useEffect(() => {
+        const detail = document.querySelector(".detail") as any;
+
+        detail.style.left = `${detailPos.x}px`;
+        detail.style.top = `${detailPos.y}px`;
+    }, [detailPos]);
     return (
         <GamePlay className="gameplay">
-            {/* <div className="top-bar relative">
-                <Gold id="gold">0</Gold>
-            </div>
-            <div className="top-center"></div>
-            <div className="bottom-bar">
-
-                <div className="status">
-                    <div className="name">Survicor's Shop</div>
-                    <div className="slider">
-                        <div className="progress">
-                            <div className="progress-bar"></div>
-                        </div>
-                    </div>
-                    <div className="others">
-                        <img src="/assets/images/rexar.png"></img>
-                        <ul className="attributes">
-                            <li>Armor: 10</li>
-                            <li>Damage: 100</li>
-                            <li>Range: 20</li>
-                        </ul>
-                    </div>
-                </div>
-
-                <div className="spec">
-                    {upgrades.map((item, index) => (
-                        <div
-                            key={`upgradeItem${index}`}
-                            className={`${
-                                (item as any).purchased
-                                    ? "opacity-0 pointer-events-none"
-                                    : ""
-                            } item`}
-                            onClick={() => onClickUpgrade(item, index)}
-                        >
-                            <img src={item.thumbnail} alt="pic" />
-                        </div>
-                    ))}
-                </div>
-            </div> */}
-
+            {/* ------- profile start --------- */}
             <div className="back fixed bottom-0 "></div>
-
             <div className="plant fixed bottom-0"></div>
-
             <div className="field fixed flex bottom-0">
                 <div className="profile flex gap-[2%]">
                     <div className="left w-[20%] flex flex-col gap-[2%]">
@@ -386,6 +585,94 @@ const GamePlayUI = ({ gameRef }: GamePlayUIProps) => {
                     </div>
                 </div>
             </div>
+            {/* ------- profile end --------- */}
+
+            {/* -------  detail start --------- */}
+            <div className="detail">
+                <div className="detail_title flex flex-row">
+                    <span>
+                        Throwing Axes ( <YellowBoldFont>Q</YellowBoldFont> )
+                    </span>
+                    <div className="price">
+                        <img src="/assets/images/coin.png"></img>
+                        <YellowBoldFont>500</YellowBoldFont>
+                    </div>
+                </div>
+                <div className="detail_data">
+                    <p>Weapon</p>
+                    <p>
+                        Damage Type: <YellowBoldFont> Normal</YellowBoldFont>
+                    </p>
+                    <p>
+                        Damage: <YellowBoldFont> 75</YellowBoldFont>
+                    </p>
+                    <p>
+                        DPS: <YellowBoldFont> 75</YellowBoldFont>
+                    </p>
+                    <p>
+                        Attack Cooldown: <YellowBoldFont> 1.0</YellowBoldFont>
+                    </p>
+                    <p>
+                        Range: <YellowBoldFont> 900</YellowBoldFont>
+                    </p>
+                    <p>
+                        Target Reference:
+                        <YellowBoldFont> Medium Armor</YellowBoldFont>
+                    </p>
+                </div>
+            </div>
+            {/* -------  detail end --------- */}
+
+            {/* -------  map start --------- */}
+            <div className="map"></div>
+            {/* -------  map end --------- */}
+
+            {/* -------  self start --------- */}
+            <div className="self">
+                <img src="/assets/images/profile.png"></img>
+                <div className="health"></div>
+                <div className="mana"></div>
+            </div>
+            {/* -------  self end --------- */}
+
+            {/* -------  player_stats start --------- */}
+            <div className="player_stats">
+                <div className="time">
+                    <YellowBoldFont>Time</YellowBoldFont>
+                    <YellowBoldFont>00:00:55</YellowBoldFont>
+                </div>
+                <div className="stats">
+                    <span>stats</span>
+                    {playerShow ? (
+                        <span className="switch">▲</span>
+                    ) : (
+                        <span className="switch">▼</span>
+                    )}
+                </div>
+                <div className="players">
+                    <table className="player_tabel">
+                        <tr>
+                            <th>Player</th>
+                            <th>Level</th>
+                            <th>Kills</th>
+                            <th>Income</th>
+                            <th>Wins</th>
+                            <th>last Stands</th>
+                        </tr>
+                        {players.map((player: PlayerData, index: number) => (
+                            <tr style={{ color: player.color }}>
+                                <td>{player.name}</td>
+                                <td>{player.level}</td>
+                                <td>{player.kills}</td>
+                                <td>{player.income}</td>
+                                <td>{player.wins}</td>
+                                <td>{player.lastStands}</td>
+                            </tr>
+                        ))}
+                    </table>
+                </div>
+            </div>
+            {/* -------  player_stats end --------- */}
         </GamePlay>
     );
 };
