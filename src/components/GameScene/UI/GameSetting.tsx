0b7907a4633db3657f9feb869f100db3208cf76e
pitervela <pitervelasquez03@gmail.com>
2023-07-25 16:42:56 -0400

    finish the gamesetting ui

--- src/components/GameScene/UI/GameSetting.tsx
+++ src/components/GameScene/UI/GameSetting.tsx
@@ -2,7 +2,8 @@ import { gsap } from "gsap";
 import { CustomEase } from "gsap/all";
 import { useEffect, useRef } from "react";
 import styled from "styled-components";
-import Checkbox from "@material-ui/core/Checkbox";
+import ReactSlider from "react-slider";
+import "../../../theme/slider.scss";
 
 import { GAME_STATES } from "../../../constants";
 
@@ -74,41 +75,35 @@ const GameSettingUI = ({ setGameState }: any) => {
     const gameSettingRef = useRef<HTMLDivElement>(null);
     const menuDownAnim = gsap.timeline();
 
-    // useEffect(() => {
-    //     if (gameSettingRef.current) {
-    //         const menu = gameSettingRef.current.childNodes[0];
+    useEffect(() => {
+        if (gameSettingRef.current) {
+            const setting = gameSettingRef.current.childNodes[0];
 
-    //         menuDownAnim.add("start").to(
-    //             menu,
-    //             {
-    //                 top: "50vh",
-    //                 duration: 1,
-    //                 ease: CustomEase.create(
-    //                     "custom",
-    //                     "M0,0,C0.266,0.412,0.666,1,0.842,1.022,0.924,1.032,0.92,1.034,1,1"
-    //                 ),
-    //             },
-    //             "start"
-    //         );
-    //     }
-    //     return () => {
-    //         menuDownAnim.kill();
-    //     };
-    // }, []);
+            menuDownAnim.add("start").from(
+                setting,
+                {
+                    top: "-50vw",
+                    duration: 1,
+                    ease: CustomEase.create(
+                        "custom",
+                        "M0,0,C0.266,0.412,0.666,1,0.842,1.022,0.924,1.032,0.92,1.034,1,1"
+                    ),
+                },
+                "start"
+            );
+        }
+        return () => {
+            menuDownAnim.kill();
+        };
+    }, []);
 
-    // const gamePlay = () => {
-    //     menuDownAnim.reverse();
-    //     if (gameSettingRef.current) {
-    //         gsap.to(gameSettingRef.current, {
-    //             backgroundColor: "#00000000",
-    //             duration: 1,
-    //             delay: 1,
-    //         });
-    //     }
-    //     gsap.delayedCall(2, () => {
-    //         setGameState(GAME_STATES.PLAYING);
-    //     });
-    // };
+    const exit = () => {
+        menuDownAnim.reverse();
+
+        // gsap.delayedCall(2, () => {
+        //     setGameState(GAME_STATES.PLAYING);
+        // });
+    };
 
     return (
         <GameSetting
@@ -127,23 +122,23 @@ const GameSettingUI = ({ setGameState }: any) => {
                         <div className="relative h-[80%] w-[80%] flex flex-col">
                             <div className="h-[20%] w-full flex justify-between items-center  ff-micro fs-md">
                                 <p className="text-white">Scrren Resolution</p>
-                                <div className="w-[35%] h-[80%] bg-[#fff4]">
+                                <div className="w-[35%] h-[80%] ">
                                     <select
                                         name="resolution"
                                         className="resolutions"
                                     >
                                         <option value="volvo">
-                                            1024 * 768
+                                            1024 x 768
                                         </option>
                                         <option value="saab">
-                                            1920 * 1080
+                                            1920 x 1080
                                         </option>
                                     </select>
                                 </div>
                             </div>
                             <div className="h-[20%] w-full flex justify-between items-center  ff-micro fs-md">
                                 <p className="text-white">Fullscreen</p>
-                                <div className="fullscreen w-[35%] h-[80%] bg-[#fff4]">
+                                <div className="fullscreen w-[35%] h-[80%] ">
                                     <div
                                         className="h-full aspect-square"
                                         style={{
@@ -153,22 +148,61 @@ const GameSettingUI = ({ setGameState }: any) => {
                                             padding: "2px",
                                         }}
                                     >
-                                        <span className="fc-orange"></span>
+                                        <svg
+                                            width="16"
+                                            height="16"
+                                            viewBox="0 0 16 16"
+                                            style={{
+                                                width: "100%",
+                                                height: "100%",
+                                                scale: "1.5",
+                                                cursor: "pointer",
+                                                // visibility: "hidden",
+                                            }}
+                                            fill="yellow"
+                                        >
+                                            <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
+                                        </svg>
                                     </div>
                                 </div>
                             </div>
-                            <div className="h-[20%] w-full flex justify-between items-center  ff-micro fs-md">
+                            <div className="h-[20%] w-full flex justify-between items-center ff-micro fs-md">
                                 <p className="text-white">Luminosity</p>
-                                <div className="w-[35%] h-[80%] bg-[#fff4]"></div>
+                                <div className="w-[35%] h-[80%]  flex items-center">
+                                    <ReactSlider
+                                        className="horizontal-slider"
+                                        thumbClassName="example-thumb"
+                                        trackClassName="example-track"
+                                        max={5}
+                                        defaultValue={[1.5]}
+                                        // renderThumb={(props, state) => <div {...props}>{state.valueNow}</div>}
+                                    />
+                                    <p className="absolute fc-orange right-0 translate-x-[30px]">
+                                        1.50
+                                    </p>
+                                </div>
                             </div>
-                            <div className="h-[20%] w-full flex justify-between items-center  ff-micro fs-md">
+                            <div className="h-[20%] w-full flex justify-between items-center ff-micro fs-md">
                                 <p className="text-white">Target Framerate</p>
-                                <div className="w-[35%] h-[80%] bg-[#fff4]"></div>
+                                <div className="w-[35%] h-[80%]  flex items-center">
+                                    <ReactSlider
+                                        className="horizontal-slider"
+                                        thumbClassName="example-thumb"
+                                        trackClassName="example-track"
+                                        max={5}
+                                        defaultValue={[1.5]}
+                                        // renderThumb={(props, state) => <div {...props}>{state.valueNow}</div>}
+                                    />
+                                    <p className="absolute fc-orange right-0 translate-x-[30px]">
+                                        1.50
+                                    </p>
+                                </div>
                             </div>
+
                             <div className="h-[20%] w-full flex justify-between items-center  ff-micro fs-md">
                                 <p className="text-white">Display FPS</p>
                                 <div
-                                    className="w-[35%] h-[80%] bg-[#fff4]"
+                                    className="w-[35%] h-[80%] "
                                     style={{
                                         borderWidth: "2px",
                                         borderColor: "#2c322f",
@@ -193,7 +227,7 @@ const GameSettingUI = ({ setGameState }: any) => {
                 <div className="buttons relative w-[50%] h-[7%] top-[20%] flex justify-between">
                     <button className="apply" />
                     <button className="default" />
-                    <button className="exit" />
+                    <button className="exit" onClick={exit} />
                 </div>
             </div>
         </GameSetting>
